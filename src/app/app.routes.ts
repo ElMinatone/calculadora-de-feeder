8571def380084b3c9f0886394f336f05f3461bf4
srmikka-admin <mickaelruteski15@gmail.com>
2025-01-20 20:39:27 -0300

    Criado novo componente de contato

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ContatoComponent } from './contato/contato.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: DashboardComponent
+    },
+    {
+        path: 'contato',
+        component: ContatoComponent
     }
 ];
